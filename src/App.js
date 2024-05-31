b90050e785df1e5727f416cca332b4464da25e8a
SUYASH AGNIHOTRI <suyashagnihotri988@gmail.com>
2024-05-31 17:10:16 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import "./App.css"
 
 function App() {
   return (
-    <div className="App h-screen w-screen flex items-center justify-center bg-gray-100  p-4 mt-10 overflow-hidden" >
+    <div className="App h-screen w-screen items-center justify-center bg-gray-100  p-4 mt-10 overflow-hidden" >
+      <h1>3-D model of room with the furniture.</h1>
       <Room />
     </div>
   );
